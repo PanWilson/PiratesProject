e05aaa525e0a39c7983e73120b5ff664d0517701
PanWilson <PanWilsonn@wp.pl>
2018-02-16 18:55:35 +0100

    Initial commit

---
+++ Source/PiratesProject/PiratesProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PiratesProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PiratesProject, "PiratesProject" );
---
+++ Source/PiratesProject/PiratesProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PiratesProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIRATESPROJECT_API APiratesProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
